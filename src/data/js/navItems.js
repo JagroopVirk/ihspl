8302d1b712a29e4dbb84cd104f6458f81e1b83cb
JagroopVirk <jd2050.1@gmail.com>
2025-06-02 19:27:35 +0530

    hero section

--- src/data/js/navItems.js
+++ src/data/js/navItems.js
@@ -1,50 +1,56 @@
-const navItems = [
-  { name: "Home", href: "/" },
-  { name: "About us", href: "/about", 
+export const navItems = [
+  { name: 'Home', href: '/' },
+  {
+    name: 'About us',
+    href: '/about',
     submenu: [
-      { name: "Vision, Mission and Quality Policy", href: "/about/vision-mission-qualityPolicy" },
-      { name: "Security and Confidentiality", href: "/about/security-confidentiality" },
-      { name: "Terms, Policy, Payments and Refunds", href: "/about/tearms&policy" },
-
+      { name: 'Vision, Mission and Quality Policy', href: '/about/vision-mission-qualityPolicy' },
+      { name: 'Security and Confidentiality', href: '/about/security-confidentiality' },
+      { name: 'Terms, Policy, Payments and Refunds', href: '/about/tearms&policy' },
     ],
   },
   {
-    name: "Services", href: "/services",
+    name: 'Services',
+    href: '/services',
     submenu: [
-      { name: "Clinical Trials", href: "/services/clinicaltrials" },
-      { name: "Bio-Analytical", href: "/services/bio-analytical" },
-      { name: "Pharmacovigilance", href: "/services/pharmacovigilance" },
-      { name: "Medical Writing", href: "/services/medicalwriting" },
-      { name: "Regulatory Services", href: "/services/regulatoryservices" },
-      { name: "Biometrics", href: "/services/biometrics" },
-      { name: "QA & Monitoring", href: "/services/qamonitoring" },
+      { name: 'Pharmacovigilance', href: '/services/pharmacovigilance' },
+      { name: 'Toxicological Risk Assessment', href: '/services/toxicological-risk-assessment' },
+      { name: 'Regulatory Services', href: '/services/regulatoryservices' },
+      { name: 'Validation', href: '/services/validation' },
+      { name: 'GMP Approvals', href: '/services/gmp-approvals' },
+      { name: 'Technology Transfer', href: '/services/technology-transfer' },
+      { name: 'Greenfield/Brownfield Projects', href: '/services/greenfield-brownfield-projects' },
+      { name: 'Calibration Lab', href: '/services/calibration-lab' },
+      { name: 'Trainings', href: '/services/trainings' },
     ],
   },
-  { name: "Resources", href: "/resources" },
-  { name: "Blogs", href: "/blogs" },
-  { name: "Career", href: "/career" },
+  { name: 'Resources', href: '/resources' },
+  { name: 'Blogs', href: '/blogs' },
+  { name: 'Career', href: '/career' },
 
   {
-    name: "Group of Companies", href: "/group-of-companies",
+    name: 'Group of Companies',
+    href: '/group-of-companies',
     submenu: [
       {
-        name: "Indivirtus Healthcare Services Pvt Ltd",
-        href: "http://www.indivirtus.com",
+        name: 'Indivirtus Solutions Pvt Ltd',
+        href: 'http://www.indivirtussolutions.com',
       },
       {
-        name: "Indivirtus Solutions Pvt Ltd",
-        href: "http://www.indivirtussolutions.com",
+        name: 'Indivirtus Strategic Solutions Pvt Ltd',
+        href: 'http://www.indivirtusrnd.com',
       },
       {
-        name: "Indivirtus Strategic Solutions Pvt Ltd",
-        href: "http://www.indivirtusrnd.com",
+        name: 'Indivirtus Global CRO Pvt Ltd',
+        href: 'http://www.indivirtuscro.com',
       },
-      { name: "Indivirtus AB7 Pvt Ltd", href: "http://www.indivirtusab7.com" },
+      {
+        name: 'Indivirtus Ecological Services Pvt Ltd',
+        href: 'http://www.indivirtuseco.com',
+      },
+      { name: 'Indivirtus AB7 Pvt Ltd', href: 'http://www.indivirtusab7.com' },
     ],
   },
 
-  { name: "Contact us", href: "/contact-us" },
-  // { name: "Current Openings", href: "/openings" },
-];
-
-export default navItems;
+  { name: 'Contact us', href: '/contact-us' },
+]
