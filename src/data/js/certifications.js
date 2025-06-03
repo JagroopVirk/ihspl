c8b48cb77692ce54b78f9e08e384b9e9e1200d78
JagroopVirk <jd2050.1@gmail.com>
2025-06-03 18:52:02 +0530

    added testimonial section

---
+++ src/data/js/certifications.js
@@ -0,0 +1,22 @@
+export const certifications = [
+  {
+    name: 'NABL Certification',
+    logo: '/assets/cert-nabl.png',
+    link: '/certificates/nabl.pdf',
+  },
+  {
+    name: 'ISO 9001:2015',
+    logo: '/assets/cert-iso.png',
+    link: '/certificates/iso9001.pdf',
+  },
+  {
+    name: 'GLP Compliance',
+    logo: '/assets/cert-glp.png',
+    link: '/certificates/glp.pdf',
+  },
+  {
+    name: 'GMP Certification',
+    logo: '/assets/cert-gmp.png',
+    link: '/certificates/gmp.pdf',
+  },
+]
