a0393ffdbc543f97c108be4f76620364315fb2cc
JagroopVirk <jd2050.1@gmail.com>
2025-06-07 13:02:14 +0530

    created social media component

---
+++ src/data/js/social.js
@@ -0,0 +1,29 @@
+import { FaTwitter, FaLinkedin, FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa6';
+
+export default [
+  {
+    link: 'https://linkedin.com/company/indivirtus',
+    label: 'LinkedIn',
+    icon: FaLinkedin,
+  },
+  {
+    link: 'https://twitter.com/indivirtus',
+    label: 'Twitter',
+    icon: FaTwitter,
+  },
+  {
+    link: 'https://facebook.com/indivirtus',
+    label: 'Facebook',
+    icon: FaFacebook,
+  },
+  {
+    link: 'https://instagram.com/indivirtus',
+    label: 'Instagram',
+    icon: FaInstagram,
+  },
+  {
+    link: 'https://whatsapp.com/indivirtus',
+    label: 'WhatsApp',
+    icon: FaWhatsapp,
+  },
+];
