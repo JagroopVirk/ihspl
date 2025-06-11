30bb6534491ff4445c0bd44a22adf68ae0a515b8
JagroopVirk <jd2050.1@gmail.com>
2025-06-11 16:58:30 +0530

    some chages

--- src/data/js/seo/siteSeo.js
+++ src/data/js/seo/siteSeo.js
@@ -1,35 +1,38 @@
 // const siteUrl = document.location.origin || "https://indivirtus.com";
-const siteUrl = "https://indivirtus.com";
-const siteLogo = siteUrl + "/assets/logo.png";
+const siteUrl = 'https://indivirtus.com';
+const siteLogo = siteUrl + '/logo.svg';
 
 export const defaultSeo = {
-  title: "Indivirtus - Contract Research Organization",
-  description: "Indivirtus is a leading Contract Research Organization (CRO) providing comprehensive clinical trial services.",
-  keywords: "website, seo, awesome",
+  title: 'Indivirtus - Contract Research Organization',
+  description:
+    'Indivirtus is a leading Contract Research Organization (CRO) providing comprehensive clinical trial services.',
+  keywords: 'website, seo, awesome',
   image: siteLogo,
   url: siteUrl,
-  twitterHandle: "@indivirtus",
-  type: "website",
+  twitterHandle: '@indivirtus',
+  type: 'website',
 };
 
 export const aboutPageSeo = {
-  title: "About Us -Indivirtus",
-  description: "Journey of Indivirtus: A leading Contract Research Organization (CRO) providing comprehensive clinical trial services.",
-  keywords: "about, mission, company",
+  title: 'About Us -Indivirtus',
+  description:
+    'Journey of Indivirtus: A leading Contract Research Organization (CRO) providing comprehensive clinical trial services.',
+  keywords: 'about, mission, company',
   image: siteLogo,
-  url: siteUrl + "/about",
-  twitterHandle: "@indivirtus",
-  type: "article",
+  url: siteUrl + '/about',
+  twitterHandle: '@indivirtus',
+  type: 'article',
   logo: siteLogo,
 };
 
 export const servicesPageSeo = {
-  title: "Services Provided -Indivirtus",
-  description: "Journey of Indivirtus: A leading Contract Research Organization (CRO) providing comprehensive clinical trial services.",
-  keywords: "about, mission, company",
+  title: 'Services Provided -Indivirtus',
+  description:
+    'Journey of Indivirtus: A leading Contract Research Organization (CRO) providing comprehensive clinical trial services.',
+  keywords: 'about, mission, company',
   image: siteLogo,
-  url: siteUrl + "/about",
-  twitterHandle: "@indivirtus",
-  type: "article",
+  url: siteUrl + '/about',
+  twitterHandle: '@indivirtus',
+  type: 'article',
   logo: siteLogo,
 };
