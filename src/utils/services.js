2130609fbae7204b72e7b928beafebb4f0ac13ac
JagroopVirk <jd2050.1@gmail.com>
2025-06-16 19:26:26 +0530

    added services list instead of dynamically adding

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,33 +1,55 @@
-import fs from 'fs';
-import path from 'path';
-import { pathToFileURL } from 'url';
-import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import path from 'path';
+// import { pathToFileURL } from 'url';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const folderPath = path.join(__dirname, '../data/js/services'); // make sure this matches your folder
+// const folderPath = path.join(__dirname, '../data/js/services'); // make sure this matches your folder
 
-/** @typedef {{ id: number, name: string }} Service */
+// /** @typedef {{ id: number, name: string }} Service */
 
-/** @type {Service[]} */
+// /** @type {Service[]} */
 
-const services = [];
+// const services = [];
 
-const files = fs.readdirSync(folderPath);
+// const files = fs.readdirSync(folderPath);
 
-for (const file of files) {
-  if (file.endsWith('.js')) {
-    const filePath = path.join(folderPath, file);
-    const fileURL = pathToFileURL(filePath);
-    const module = await import(fileURL);
+// for (const file of files) {
+//   if (file.endsWith('.js')) {
+//     const filePath = path.join(folderPath, file);
+//     const fileURL = pathToFileURL(filePath);
+//     const module = await import(fileURL);
 
-    if (module && module.default) {
-      services.push(module.default);
-    } else {
-      console.warn(`File ${file} has no default export`);
-    }
-  }
-}
+//     if (module && module.default) {
+//       services.push(module.default);
+//     } else {
+//       console.warn(`File ${file} has no default export`);
+//     }
+//   }
+// }
+
+import adepde from '@/data/js/services/adepde';
+import calibration from '@/data/js/services/calibration';
+import gmpAudits from '@/data/js/services/gmpAudits';
+import gmpComplianceTrainings from '@/data/js/services/gmpComplianceTrainings';
+import greenfieldBrownfield from '@/data/js/services/greenfieldBrownfield';
+import pharmacovigilance from '@/data/js/services/pharmacovigilance';
+import regulatoryAffairs from '@/data/js/services/regulatoryAffairs';
+import riskAssessment from '@/data/js/services/riskAssessment';
+import validation from '@/data/js/services/validation';
+
+const services = [
+  adepde,
+  calibration,
+  gmpAudits,
+  gmpComplianceTrainings,
+  greenfieldBrownfield,
+  pharmacovigilance,
+  regulatoryAffairs,
+  riskAssessment,
+  validation,
+];
 
 export default services;
