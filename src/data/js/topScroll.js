eb9acdea7c987711b8ec5dd6f7edd5c59b0533a2
JagroopVirk <jd2050.1@gmail.com>
2025-06-30 18:16:08 +0530

    added emoji on top scroll

--- src/data/js/topScroll.js
+++ src/data/js/topScroll.js
@@ -3,9 +3,10 @@ import { FaPhone, FaEnvelope } from 'react-icons/fa6';
 const topScroll = {
   scrollText:
     'We are an integral part of Indivirtus Global CRO group of companies, where every service domain is led by group of experts to provide their quality, knowledge, and expertise. We offer end-to-end clinical research solutions across the globe including Clinical Trials (Phase I-IV), BA / BE studies, Clinical Endpoint studies, and clinical research solutions from PK studies in healthy volunteers to patient trials for generics and NCEs.',
-  row1: 'Queries related to any service or business: - +91 9888111109, +91 91319 25456, director.bd@indivirtus.com, director@indivirtus.com',
-  row2: 'For Other enquiries - +91 62809 18142, admin@indivirtus.com',
-  row3: 'For Job and vacancy related enquiries- hr@indivirtus.com',
+  row1: 'Queries related to any service or business: - <a class="scrollLink" href="tel:+919888111109">&#9990 +91 9888111109</a>, <a class="scrollLink" href="tel:+919131925456">&#9990 +91 91319 25456</a>, <a class="scrollLink" href="mailto:director.bd@indivirtus.com">&#9993 director.bd@indivirtus.com</a>, <a class="scrollLink" href="mailto:director@indivirtus.com">&#9993 director@indivirtus.com</a>',
+  row2: 'For Other enquiries - <a class="scrollLink" href=tel:+916280918142">&#9990 +91 62809 18142</a>, <a class="scrollLink" href="mailto:admin@indivirtus.com">&#9993 admin@indivirtus.com</a>',
+  row3: 'For Job and vacancy related enquiries- <a class="scrollLink" href="mailto:hr@indivirtus.com">&#9993 hr@indivirtus.com</a>',
+  icon: FaPhone,
 };
 
 export default topScroll;
