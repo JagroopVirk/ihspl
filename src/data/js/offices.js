88c10c1a75df7e157c05b06b7edb9d8ae92b7a34
JagroopVirk <jd2050.1@gmail.com>
2025-06-30 17:13:44 +0530

    updated footer with addresses

--- src/data/js/offices.js
+++ src/data/js/offices.js
@@ -1,28 +1,61 @@
-const offices = [
-  {
-    location: 'INDIA- Head Office',
-    address: '521-522, Top Floor, Taj Plaza, TDI City, Sector-118,  Airport Road, Mohali, Punjab, India 160059',
-    phone: ['+91 9131925456', '+91 9888111109', '+91 172 4508817'],
-    email: ['director.bd@indivirtus.com', 'director@indivirtus.com'],
-  },
-  {
-    location: 'UAE',
-    address: '502, Saleh Bin Lahej 341,Al Barsha 1, Dubai UAE, 345055',
-    phone: ['+971 5250 61253'],
-    email: ['support.uae@indivirtus.com'],
-  },
+// const internationOffices = [
+//   {
+//     location: 'INDIA- Head Office',
+//     address: '521-522, Top Floor, Taj Plaza, TDI City, Sector-118,  Airport Road, Mohali, Punjab, India 160059',
+//     phone: ['+91 9131925456', '+91 9888111109', '+91 172 4508817'],
+//     email: ['director.bd@indivirtus.com', 'director@indivirtus.com'],
+//   },
+//   {
+//     location: 'UAE',
+//     address: '502, Saleh Bin Lahej 341,Al Barsha 1, Dubai UAE, 345055',
+//     phone: ['+971 5250 61253'],
+//     email: ['support.uae@indivirtus.com'],
+//   },
 
+//   {
+//     location: 'UAE',
+//     address: '502, Saleh Bin Lahej 341,Al Barsha 1, Dubai UAE, 345055',
+//     phone: ['+971 5250 61253'],
+//     email: ['support.uae@indivirtus.com'],
+//   },
+//   {
+//     location: 'INDIA- Head Office',
+//     address: '521-522, Top Floor, Taj Plaza, TDI City, Sector-118,  Airport Road, Mohali, Punjab, India 160059',
+//     phone: ['+91 9131925456', '+91 9888111109', '+91 172 4508817'],
+//     email: ['director.bd@indivirtus.com', 'director@indivirtus.com'],
+//   },
+//   {
+//     location: 'U K',
+//     address: '16 Bissley Drive,Maidenhead, Berkshire SL 6 3UZ',
+//     phone: ['+44 20 3286 0405'],
+//     email: ['support.uk@indivirtus.com'],
+//   },
+//   {
+//     location: 'USA',
+//     address: '23950 Mondavi Drive, Novi MI 48374, USA',
+//     phone: ['+1 (323) 522-5967'],
+//     email: ['support.usa@indivirtus.com'],
+//   },
+//   {
+//     location: 'EGYPT',
+//     address: '5 El Nasr Street, Sheraton Heliopolis El Nozha Cairo, Egypt',
+//     phone: ['+20 (106) 082-7300'],
+//     email: ['director.bd@indivirtus.com'],
+//   },
+//   {
+//     location: 'CANADA',
+//     address: '2 County Court Blvd.Suite 400, Brampton, ON L6W 3W8, Canada',
+//     phone: ['+1 (647) 581-6157'],
+//     email: ['support.ca@indivirtus.com'],
+//   },
+// ];
+
+const internationOffices = [
   {
     location: 'UAE',
     address: '502, Saleh Bin Lahej 341,Al Barsha 1, Dubai UAE, 345055',
     phone: ['+971 5250 61253'],
     email: ['support.uae@indivirtus.com'],
-  },
-  {
-    location: 'INDIA- Head Office',
-    address: '521-522, Top Floor, Taj Plaza, TDI City, Sector-118,  Airport Road, Mohali, Punjab, India 160059',
-    phone: ['+91 9131925456', '+91 9888111109', '+91 172 4508817'],
-    email: ['director.bd@indivirtus.com', 'director@indivirtus.com'],
   },
   {
     location: 'U K',
@@ -50,4 +83,27 @@ const offices = [
   },
 ];
 
-export default offices;
+const domesticOffices = [
+  {
+    location: 'Registered Office',
+    address: '5020 A, Sector 38 West, Chandigarh, India 160055',
+    phone: ['+91 172 4508817'],
+    email: ['admin@indivirtus.com'],
+    seq: { small: 2, big: 1 },
+  },
+  {
+    location: 'INDIA- Head Office',
+    address: '521-522, Top Floor, Taj Plaza, TDI City, Sector-118,  Airport Road, Mohali, Punjab, India 160059',
+    phone: ['+91 9131925456', '+91 9888111109'],
+    email: ['director.bd@indivirtus.com', 'director@indivirtus.com'],
+    seq: { small: 1, big: 2 },
+  },
+  {
+    location: 'Raipur',
+    address: '404- Fourth Floor, Avinash Time Square, Sector - 19, Nawa Raipur, Chhattisgarh, India ',
+    phone: ['+971 5250 61253'],
+    seq: { small: 3, big: 3 },
+  },
+];
+
+export { internationOffices, domesticOffices };
