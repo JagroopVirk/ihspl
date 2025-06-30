2959f2410ed788a2b13b625a0e77f0caa5a6f063
JagroopVirk <jd2050.1@gmail.com>
2025-06-30 16:13:37 +0530

    update who we are

---
+++ src/data/js/seo/homePage.js
@@ -0,0 +1,106 @@
+const link = '/';
+
+export default {
+  gtm: {
+    eventCategory: '',
+    eventAction: '',
+    eventLabel: '',
+    customDimensions: {},
+    dataLayerPush: {},
+  },
+  schema: {
+    '@context': 'https://schema.org',
+    '@type': 'ProfessionalService',
+    name: '',
+    description: '',
+    serviceType: '',
+    provider: {
+      '@type': 'Organization',
+      name: 'Indivirtus',
+      url: 'https://www.indivirtus.com',
+      logo: 'https://www.indivirtus.com/logo.png',
+      sameAs: [
+        'https://www.linkedin.com/company/indivirtus-group-of-companies',
+        'https://twitter.com/indivirtus',
+        'https://www.facebook.com/Indivirtus',
+        'https://www.instagram.com/indivirtus_healthcare',
+      ],
+    },
+    areaServed: {
+      '@type': 'Country',
+      name: 'Global',
+    },
+    availableChannel: {
+      '@type': 'ServiceChannel',
+      serviceUrl: link,
+      servicePhone: '+91-9131925456',
+      servicePostalAddress: {
+        '@type': 'PostalAddress',
+        streetAddress: '522, Taj Plaza, TDI city, Sector 118',
+        addressLocality: 'Mohali',
+        addressRegion: 'Punjab',
+        postalCode: '160059',
+        addressCountry: 'India',
+      },
+    },
+    hasOfferCatalog: {
+      '@type': 'OfferCatalog',
+      name: '',
+      itemListElement: [
+        {
+          '@type': 'Offer',
+          itemOffered: {
+            '@type': 'Service',
+            name: '',
+            description: '',
+          },
+        },
+      ],
+    },
+    offers: {
+      '@type': 'Offer',
+      priceCurrency: ['INR', 'USD'],
+      price: 'Compound-based pricing',
+      url: link,
+      availability: 'https://schema.org/InStock',
+      businessFunction: 'https://schema.org/ProvideService',
+    },
+  },
+  faqSchema: {
+    '@context': 'https://schema.org',
+    '@type': 'FAQPage',
+    mainEntity: [
+      {
+        '@type': 'Question',
+        name: '',
+        acceptedAnswer: {
+          '@type': 'Answer',
+          text: '',
+        },
+      },
+    ],
+  },
+  meta: {
+    title: '',
+    description: '',
+    keywords: [],
+    ogTags: {
+      type: 'service',
+      image: serviceImage.src,
+      url: link,
+      site_name: 'Indivirtus',
+    },
+    twitterCard: {
+      card: 'summary_large_image',
+      title: '',
+      description: '',
+      image: serviceImage.src,
+    },
+    canonicalUrl: `'https://www.indivirtus.com' + ${link}`,
+    robotsMeta: {
+      index: true,
+      follow: true,
+      nocache: false,
+    },
+  },
+};
