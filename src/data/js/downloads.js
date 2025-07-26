661352cbad873d94003aa13822d9d6b4232c7ef4
JagroopVirk <jd2050.1@gmail.com>
2025-07-26 12:32:36 +0530

    updated downloads

--- src/data/js/downloads.js
+++ src/data/js/downloads.js
@@ -1,13 +1,42 @@
 export const downloads = [
-  { title: 'Indivirtus Healthcare Services', fileUrl: '/downloads/Indivirtus_Brochure.pdf' },
-  { title: 'Indivirtus Solutions', fileUrl: '/downloads/Brochure_Indivirtus-Solutions.pdf' },
-  { title: 'Indivirtus Strategic Solutions', fileUrl: '/downloads/Indivirtus_Brochure.pdf' },
-  { title: 'Indivirtus Calibration Services', fileUrl: '/downloads/calibration_brochure.pdf' },
+  {
+    title: 'Indivirtus Healthcare Services',
+    fileUrl: '/downloads/Indivirtus_Brochure.pdf',
+    aria: 'Indivirtus Healthcare Services Brochure',
+  },
+  {
+    title: 'Indivirtus Solutions',
+    fileUrl: '/downloads/Brochure_Indivirtus-Solutions.pdf',
+    aria: 'Indivirtus Solutions Brochure',
+  },
+  {
+    title: 'Indivirtus Strategic Solutions',
+    fileUrl: '/downloads/Indivirtus_Brochure.pdf',
+    aria: 'Indivirtus Strategic Solutions Brochure',
+  },
+  {
+    title: 'Indivirtus Calibration Services',
+    fileUrl: '/downloads/calibration_brochure.pdf',
+    aria: 'Indivirtus Calibration Services Brochure',
+  },
   {
     title: 'Comprehensive Quotation for all PV activities (EURO)',
     fileUrl: '/downloads/Comprehensive-quotation-for-all-PV-activities-06.10.2023-EURO.pdf',
+    aria: 'Comprehensive Quotation for all PV activities (EURO)',
   },
-  { title: 'TT Products-R&D Revised_EU', fileUrl: '/downloads/TT-Products-RD-Revised_EU-updated-compressed.pdf' },
-  { title: 'Sample Lacosamide (Oral) PDE+OEL', fileUrl: '/downloads/Sample-Lacosamide-Oral-PDEOEL.pdf' },
-  { title: 'List of completed Clinical Trials', fileUrl: '/downloads/List-of-completed-Clinical-Trials-1.pdf' },
+  {
+    title: 'TT Products-R&D Revised_EU',
+    fileUrl: '/downloads/TT-Products-RD-Revised_EU-updated-compressed.pdf',
+    aria: 'TT Products-R&D EU',
+  },
+  {
+    title: 'Sample Lacosamide (Oral) PDE+OEL',
+    fileUrl: '/downloads/Sample-Lacosamide-Oral-PDEOEL.pdf',
+    aria: 'Sample Lacosamide (Oral) PDE+OEL',
+  },
+  {
+    title: 'List of completed Clinical Trials',
+    fileUrl: '/downloads/List-of-completed-Clinical-Trials-1.pdf',
+    aria: 'List of completed Clinical Trials',
+  },
 ];
