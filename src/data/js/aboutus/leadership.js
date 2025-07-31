5d7fae436f62ce9ff23a6214b2355bd5c5e6f73e
JagroopVirk <jd2050.1@gmail.com>
2025-07-31 16:01:34 +0530

    added leadership

---
+++ src/data/js/aboutus/leadership.js
@@ -0,0 +1,58 @@
+import ujPic from '@/assets/leadership/Dr._Upendra_K_Jain.png';
+import vkPic from '@/assets/leadership/Ms._Veena_Kumari.jpg';
+import ajPic from '@/assets/leadership/Dr._Ashish_Jain.jpg';
+import rkPic from '@/assets/leadership/Dr._Randeep_Signh.jpg';
+
+export default [
+  {
+    name: 'Dr. Upendra Jain',
+    title: 'Chief Executive Officer',
+    image: ujPic,
+    bg: 'bg-pink-100',
+    description: {
+      name: 'Dr. Upendra Jain',
+      qualification: 'M.Pharm, PhD',
+      details: [
+        'is  a  distinguished  professional  with  an  impressive  career spanning over 32 years. As the Chief Executive Officer of a leading  organization,  he  brings  a  wealth  of  expertise  and  a proven track record of success.With  a  solid  foundation  in  pharmaceuticals,  Dr.  Jain  has dedicated 15 years to honing his skills in Healthcare Industry, Pharmaceutical  Sales  and  Marketing.  During  this  period,  he demonstrated exceptional leadership, strategic thinking, and a deep  understanding  of  the  industry  dynamics,  contributing significantly  to  the  growth  and  market presence  of  the organizations he has been associated with.',
+        'In addition to his extensive experience in the pharmaceutical sector, Dr. Jain has also spent 12 years in the field of education. Notably,  he  served  as  the  Director  for  Chandigarh  Group  of Colleges, where his visionary leadership played a pivotal role in shaping the academic landscape. His commitment to fostering knowledge and innovation has made a lasting impact on the next generation of professionals',
+      ],
+    },
+  },
+  {
+    name: 'Ms. Veena Kanwar',
+    title: 'Managing Director',
+    image: vkPic,
+    bg: 'bg-purple-100',
+    description: {
+      name: 'Ms. Veena Kanwar',
+      qualification: 'MBA (HR & IT)',
+      details: [
+        'is  an  accomplished  Managing  Director,  brings  25  years  of profound expertise in Human Resources and Administration to her dynamic role. Having served as the Director of Administration at Chandigarh Group of Colleges, she has showcased strategic leadership,  innovative  HR  practices,  and  an  unwavering commitment to fostering an inclusive workplace culture.',
+        "Ms. Kanwar's unique skill set, merging HR and IT, reflects her dynamic approach to driving organizational success. Her visionary leadership extends beyond the professional realm, as she actively engages in community initiatives, aligning the organization with impactful CSR projects.",
+        "As a thought leader and advocate for corporate responsibility, Ms.Kanwar's  holistic  leadership  style  has  positioned  her  as  a respected figure, contributing significantly to both corporate and community  spheres.  Her  dedication  to  innovation,  inclusive culture, and community impact defines her as a transformative leader in the realms of HR and Administration.",
+      ],
+    },
+  },
+  {
+    name: 'Dr. Ashish Jain',
+    title: 'Director - Business Development',
+    image: ajPic,
+    bg: 'bg-blue-100',
+    description: {
+      name: 'Dr. Ashish Jain',
+      qualification: '',
+      details: ['22+ Years of experience in Marketing & Business Development in Domestic & Export'],
+    },
+  },
+  {
+    name: 'Dr. Randeep Singh',
+    title: 'Director - Global Business Development',
+    image: rkPic,
+    bg: 'bg-yellow-100',
+    description: {
+      name: 'Dr. Randeep Singh',
+      qualification: '',
+      details: ['22+ years of experience in Marketing & Business Development in Domestic & Export'],
+    },
+  },
+];
